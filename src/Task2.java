0d4f1b36a06040f30d12c327cd9bb6574af1b232
ignatKupryashin <ikupryashin@gmail.com>
2022-12-04 02:28:41 +0300

    сделал вторую задачу

--- src/Task2.java
+++ src/Task2.java
@@ -4,10 +4,40 @@
 //        first() - возвращает первый элемент из очереди, не удаляя.
 
 
+import java.util.LinkedList;
+import java.util.Random;
 
 public class Task2 {
     public static void main(String[] args) {
+    LinkedList <Integer> testList = new LinkedList<>();
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            testList.add(i, rand.nextInt(100));
+        }
 
+        //запуски и проверки
+        System.out.println(testList);
+        enqueue(testList,123);
+        System.out.println(testList);
+        int myValue = dequeue(testList);
+        System.out.println(myValue);
+        System.out.println(testList);
+        myValue =  first(testList);
+        System.out.println(myValue);
+        System.out.println(testList);
     }
 
+    public static void enqueue(LinkedList myList, int value) {;
+        myList.add(value);
+    }
+
+    public static int dequeue(LinkedList myList) {
+        int answer = (Integer) myList.get(0);
+        myList.remove(0);
+        return answer;
+    }
+
+    public static int first(LinkedList myList) {
+        return (Integer) myList.get(0);
+    }
 }
